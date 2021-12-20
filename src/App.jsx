40e43f18886bae46c26e73128045b66d1974e962
GoldLion <hotgold0905@gmail.com>
2021-12-20 14:22:38 +0900

    Fix: Check Ethereum

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const checkIfWalletIsConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={null}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
