5b1df7bbf5d5f9887b77b11e6dffa75829a6bd0f
Holmes-7 <haodev007@gmail.com>
2021-12-20 14:28:11 +0900

    Feat: Add My Name

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <div className="mainContainer">
       <div className="dataContainer">
-        <div className="header">👋 Hey there!</div>
+        <div className="header">👋 Hey there! I am Ming.HaoQing</div>
 
         <div className="bio">
           I am farza and I worked on self-driving cars so that's pretty cool
