837810925db3ab5544b79647d9d3043add7fc451
Holmes-7 <haodev007@gmail.com>
2021-12-20 16:19:31 +0900

    Feat: Metamask Connect Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const checkIfWalletIsConnected = () => {
-    const { ethereum } = window;
+  const [currentAccount, setCurrentAccount] = useState("");
 
-    if (!ethereum) {
-      console.log("Make sure you have metamask!");
-      return;
-    } else {
-      console.log("We have the ethereum object", ethereum);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts !== 0) {
+        const account = accounts[0];
+        console.log("Fount an authorized account", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -30,6 +66,12 @@ const App = () => {
         <button className="waveButton" onClick={null}>
           Wave at Me
         </button>
+        {currentAccount && (
+          <button
+            className="waveButton"
+            onClick={() => connectWallet()}
+          ></button>
+        )}
       </div>
     </div>
   );
