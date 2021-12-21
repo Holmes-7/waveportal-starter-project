f7845c9ae429c98dab39448b6722d871f86a0029
Holmes-7 <haodev007@gmail.com>
2021-12-21 11:22:12 +0900

    Add wave function

--- src/App.jsx
+++ src/App.jsx
@@ -54,33 +54,32 @@ const App = () => {
     }
   };
 
-  // const wave = async () => {
-  //   try {
-  //     const { ethereum } = window;
-
-  //     if (ethereum) {
-  //       const provider = new ethers.providers.Web3Provider(ethereum);
-  //       const signer = provider.getSigner();
-  //       const wavePortalContract = new ethers.Contract(
-  //         contractAddress,
-  //         contractABI,
-  //         signer
-  //       );
-  //       let count = wavePortalContract.getWaveTotal();
-  //       console.log("Retrived wave count: ", count.toNumber());
-  //       const waveTxn = await wavePortalContract.wave();
-  //       console.log("Mining...", waveTxn.hash);
-  //       await waveTxn.wait();
-  //       console.log("Mined --", waveTxn.hash);
-  //       count = wavePortalContract.getWaveTotal();
-  //       console.log("Retreived total wave count: ", count);
-  //     } else {
-  //       console.log("Ethereum object doesn't exist");
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+  const wave = async () => {
+    //   try {
+    //     const { ethereum } = window;
+    //     if (ethereum) {
+    //       const provider = new ethers.providers.Web3Provider(ethereum);
+    //       const signer = provider.getSigner();
+    //       const wavePortalContract = new ethers.Contract(
+    //         contractAddress,
+    //         contractABI,
+    //         signer
+    //       );
+    //       let count = wavePortalContract.getWaveTotal();
+    //       console.log("Retrived wave count: ", count.toNumber());
+    //       const waveTxn = await wavePortalContract.wave();
+    //       console.log("Mining...", waveTxn.hash);
+    //       await waveTxn.wait();
+    //       console.log("Mined --", waveTxn.hash);
+    //       count = wavePortalContract.getWaveTotal();
+    //       console.log("Retreived total wave count: ", count);
+    //     } else {
+    //       console.log("Ethereum object doesn't exist");
+    //     }
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
